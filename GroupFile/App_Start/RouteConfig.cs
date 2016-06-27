5d8deb369c40eef723be44c37cea0f727450007a
VuNM <manhvu1212@gmail.com>
2016-06-27 22:25:24 +0700

    change default router

--- GroupFile/App_Start/RouteConfig.cs
+++ GroupFile/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GroupFile
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Files", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ GroupFile/Controllers/FilesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace GroupFile.Controllers
+{
+    public class FilesController : Controller
+    {
+        public static readonly string PATH_ROOT_UPLOADED = System.Web.HttpContext.Current.Server.MapPath("~/rootuploaded");
+
+        // GET: Files
+        public ActionResult Index()
+        {
+            List<string> listFile = ProcessDirectory(PATH_ROOT_UPLOADED);
+            return View(listFile);
+        }
+
+        public List<string> ProcessDirectory(string pathfile)
+        {
+            List<string> listFile = new List<string>();
+
+            foreach (string directory in Directory.GetDirectories(pathfile))
+            {
+                listFile.AddRange(ProcessDirectory(directory));
+            }
+            
+            try
+            {
+                foreach (string file in Directory.GetFiles(pathfile))
+                {
+                    listFile.Add(Path.GetFullPath(file));
+                }
+            }
+            catch (DirectoryNotFoundException e)
+            {
+
+            }
+
+            return listFile;
+        }
+    }
+}
